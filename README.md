d5d8e6f6ad60736b27c893a5791069eb0cf8bb46
misetteichan <190587230+misetteichan@users.noreply.github.com>
2024-12-11 20:29:03 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MisetteichanStyle
+
+ Library for misetteichan!
---
+++ src/MisetteichanStyle.h
@@ -0,0 +1,26 @@
+// Copyright (c) 2024 misetteichan
+// Licensed under the MIT License. See LICENSE file for details.
+
+#ifndef MISETTEICHANSTYLE_H__
+#define MISETTEICHANSTYLE_H__
+
+#include <M5Unified.h>
+
+namespace mstc_style {
+
+namespace port {
+  constexpr m5::pin_name_t a_outside = m5::pin_name_t::port_a_pin1;
+  constexpr m5::pin_name_t a_inside = m5::pin_name_t::port_a_pin2;
+  constexpr m5::pin_name_t b_outside = m5::pin_name_t::port_b_pin1;
+  constexpr m5::pin_name_t b_inside = m5::pin_name_t::port_b_pin2;
+  constexpr m5::pin_name_t c_outside = m5::pin_name_t::port_c_pin1;
+  constexpr m5::pin_name_t c_inside = m5::pin_name_t::port_c_pin2;
+  constexpr m5::pin_name_t d_outside = m5::pin_name_t::port_d_pin1;
+  constexpr m5::pin_name_t d_inside = m5::pin_name_t::port_d_pin2;
+  constexpr m5::pin_name_t e_outside = m5::pin_name_t::port_e_pin1;
+  constexpr m5::pin_name_t e_inside = m5::pin_name_t::port_e_pin2;
+}
+
+}
+
+#endif  // MISETTEICHANSTYLE_H__
